4db37fda28e6b2781fed5d221ed592849a5c6c71
ZuoYouheng <zyh201409@gmail.com>
2022-03-22 11:52:24 +0800

    增大了文字和按钮

--- Experiments/jspsych/plugin-survey-likert.js
+++ Experiments/jspsych/plugin-survey-likert.js
@@ -92,15 +92,15 @@ var jsPsychSurveyLikert = (function (jspsych) {
           // inject CSS for trial
           html += '<style id="jspsych-survey-likert-css">';
           html +=
-              ".jspsych-survey-likert-statement { display:block; font-size: 16px; padding-top: 40px; margin-bottom:10px; }" +
+              ".jspsych-survey-likert-statement { display:block; font-size: 28px; padding-top: 40px; margin-bottom:10px; }" +
                   ".jspsych-survey-likert-opts { list-style:none; width:" +
                   w +
-                  "; margin:auto; padding:0 0 35px; display:block; font-size: 14px; line-height:1.1em; }" +
+                  "; margin:auto; padding:0 50px 35px; display:block; font-size: 24px; line-height:1.1em; }" +
                   ".jspsych-survey-likert-opt-label { line-height: 1.1em; color: #444; }" +
-                  ".jspsych-survey-likert-opts:before { content: ''; position:relative; top:11px; /*left:9.5%;*/ display:block; background-color:#efefef; height:4px; width:100%; }" +
+                  ".jspsych-survey-likert-opts:before { content: ''; position:relative; top:23px; /*left:9.5%;*/ display:block; background-color:#efefef; height:8px; width:100%; }" +
                   ".jspsych-survey-likert-opts:last-of-type { border-bottom: 0; }" +
                   ".jspsych-survey-likert-opts li { display:inline-block; /*width:19%;*/ text-align:center; vertical-align: top; }" +
-                  ".jspsych-survey-likert-opts li input[type=radio] { display:block; position:relative; top:0; left:50%; margin-left:-6px; }";
+                  ".jspsych-survey-likert-opts li input[type=radio] { display:block; position:relative; top:0; left:50%; margin-left:-20px; width:30px; height:30px}";
           html += "</style>";
           // show preamble text
           if (trial.preamble !== null) {
